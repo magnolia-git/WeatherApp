d10e9b2515a527a1cd740ec98111de7557066f8f
Travis Wombles <womblestravis@gmail.com>
2021-01-19 03:40:36 -0600

    added things

--- src/index.js
+++ src/index.js
@@ -2,16 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import About from './About';
+import Weather from './Weather';
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const routing = (
+  <Router>
+    <div>
+      <h2>React Weather App</h2>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/Weather">Weather</Link>
+        </li>
+      </ul>
+      <Route exact path="/" component={App} />
+      <Route path="/about" component={About} />
+      <Route path="/Weather" component={Weather} />
+    </div>
+  </Router>
+)
+ReactDOM.render(routing, document.getElementById('root'));
